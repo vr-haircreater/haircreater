a40c4d426cd45c5e5c698272edc80e91e4fef43c
iamsherry000 <iamsherry000@gmail.com>
2021-07-25 10:32:50 +0800

    Sherry0725_1032

---
+++ Assets/Scripts/Else/TryFunctions/functions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functions : MonoBehaviour
+{
+    public List<GameObject> ListCubes = new List<GameObject>();
+    Stack<GameObject> StackCubes = new Stack<GameObject>();
+    //GameObject haha = new GameObject();
+    GameObject re = new GameObject();
+    int ur = 0;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = new GameObject(); 
+            ListCubes.Add(go);
+            ListCubes[ListCubes.Count - 1].name = "go" + (ListCubes.Count - 1);
+            ur = 0;
+        }
+
+        if (Input.GetKeyDown("u"))
+        {
+            ur = 1;
+            re = Instantiate(ListCubes[ListCubes.Count - 1]);
+            StackCubes.Push(re);
+            re.SetActive(false);
+            Destroy(ListCubes[ListCubes.Count - 1]);
+            ListCubes.RemoveAt(ListCubes.Count - 1);
+        }
+
+        if (Input.GetKeyDown("r") && ur == 1)
+        {
+            GameObject haha;
+            haha = StackCubes.Pop();
+            ListCubes.Add(haha);
+            haha.SetActive(true);
+            if (StackCubes.Count == 0) ur = 0;
+        }
+    }
+}
