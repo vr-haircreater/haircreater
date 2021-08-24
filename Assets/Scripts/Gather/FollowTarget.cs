d53d2fee9d874a4039e6bab3fe57d1978ae68ccd
unknown <yullwa1508@gmail.com>
2021-08-24 21:55:13 +0800

    add追縱

--- Assets/Scripts/Gather/FollowTarget.cs
+++ Assets/Scripts/Gather/FollowTarget.cs
@@ -12,11 +12,17 @@ public class FollowTarget : MonoBehaviour
     {
         navAgent = GetComponent<NavMeshAgent>();
         //navAgent.SetDestination(target.transform.position);
-        
+
     }
 
     void Update()
     {
+       
         navAgent.SetDestination(target.transform.position);
+        navAgent.speed = 1.5f;
+        navAgent.autoBraking = true;
+        navAgent.stoppingDistance = 1;
+        navAgent.height = 0.8f;
+        navAgent.radius = 0.3f;
     }
 }
